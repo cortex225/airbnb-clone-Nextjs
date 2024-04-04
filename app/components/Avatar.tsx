6c4efe55a11293f480ef8d415b855dd0e224f542
jlgouaho <1900751@CegepGranby.qc.ca>
2024-04-04 11:46:13 -0400

    init

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,15 +1,17 @@
 "use client";
 import Image from "next/image";
+
 const Avatar = () => {
-  return (
-    <Image
-      width="30"
-      height="30"
-      alt="avatar"
-      className="rounded-full"
-      src="/images/placeholder.jpg"
-    />
-  );
+    return (
+        <Image
+            width="30"
+            height="30"
+            alt="avatar"
+            className="rounded-full "
+      
+            src="/images/placeholder.jpg"
+        />
+    );
 };
 
 export default Avatar;
